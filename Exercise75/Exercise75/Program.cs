7fe5e0449f2c3a1bf55dfa976fad2debff326c36
brooke <brookedanae88@gmail.com>
2021-03-12 14:48:01 -0500

    Final commit

---
+++ Exercise75/Exercise75/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise75
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            var userNum = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Your square root is: " + SquareRoot(userNum, 0));
+        }
+
+        public static int SquareRoot(int number, int rt)
+        {
+            if (rt > number / 2)
+            {
+                return -1;
+            }
+            else if (rt * rt == number)
+            {
+                return rt;
+            }
+            else
+            {
+                rt++;
+                return SquareRoot(number, rt);
+            }
+
+        }
+    }
+}
